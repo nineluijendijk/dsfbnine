3b09d9c1caec66359ea219d4ded7a875eafe8015
Nine Luijendijk <nineluijendijk@MacBook-Pro-van-Nine.local>
2023-01-05 20:38:46 +0100

    first function

---
+++ R/gotermAnalysis.R
@@ -0,0 +1,37 @@
+#' Perform a GO-term enrichment analysis
+#'
+#' @param dge_results Object of class "DESeqResults".
+#' @param L2FC Value of the Log2FoldChange to determine what genes are differently regulated.
+#' @param padjusted Value of the adjusted p-value to determine what genes are differently regulated.
+#' @param ontologytype Type of ontology to search for. Options: "BP", "CC", "MF".
+#' @param pcutoff Value of what p-values to filter by.
+#' @param upregulated Boolean stating whether to look for the upregulated or downregulated genes.
+#'
+#' @return A dataframe.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' results_dge <- DESeq2::results(DESeq2::DESeq(DESeqDataSet))
+#' gotermAnalysis(results_dge, ontologytype = "BP", upregulated = FALSE)
+#' }
+gotermAnalysis <- function(dge_results, L2FC = 1, padjusted = 0.01, ontologytype = "BP", pcutoff = 1, upregulated = TRUE) {
+  if(upregulated == TRUE){
+    regulated_genes <- dge_results %>% data.frame() %>%
+      dplyr::filter(log2FoldChange > L2FC, padj < padjusted) %>% rownames()
+  } else {
+    regulated_genes <- dge_results %>% data.frame() %>%
+      dplyr::filter(log2FoldChange < -L2FC, padj < padjusted) %>% rownames()
+  }
+
+  all_genes <- dge_results %>% data.frame() %>% rownames()
+  test_object <- methods::new("GOHyperGParams",
+                              geneIds = regulated_genes,
+                              universeGeneIds = all_genes,
+                              annotation = "org.Hs.eg.db",
+                              ontology = ontologytype,
+                              pvalueCutoff = pcutoff,
+                              testDirection = "over")
+
+  summary(GOstats::hyperGTest(test_object))
+}
---
+++ temp.R
@@ -0,0 +1,53 @@
+library(DESeq2)
+library("org.Hs.eg.db")
+
+resultsdge <- readRDS(here("dge_results"))
+
+gotermAnalysis <- function(dge_results, L2FC = 1, padjusted = 0.01, ontologytype = "BP", pcutoff = 1, upregulated = TRUE) {
+  if(upregulated == TRUE){
+    regulated_genes <- dge_results %>% data.frame() %>%
+      dplyr::filter(log2FoldChange > L2FC, padj < padjusted) %>% rownames()
+  } else {
+    regulated_genes <- dge_results %>% data.frame() %>%
+      dplyr::filter(log2FoldChange < -L2FC, padj < padjusted) %>% rownames()
+  }
+
+  all_genes <- dge_results %>% data.frame() %>% rownames()
+  test_object <- methods::new("GOHyperGParams",
+                     geneIds = regulated_genes,
+                     universeGeneIds = all_genes,
+                     annotation = "org.Hs.eg.db",
+                     ontology = ontologytype,
+                     pvalueCutoff = pcutoff,
+                     testDirection = "over")
+
+  summary(GOstats::hyperGTest(test_object))
+}
+
+gotermPlot <- function(goterm_results, padj_method = "BH", filter_from = 5, filter_to = 500, topamount = 10, plot_title) {
+  goterm_results$padj <- p.adjust(goterm_results$Pvalue, method = padj_method)
+  goterm_results <- goterm_results %>% dplyr::filter(Count > filter_from) %>% dplyr::filter(Count < filter_to)
+  goterm_results_top <- goterm_results[order(goterm_results$padj)[1:topamount],]
+  goterm_results_top$Term <- factor(goterm_results_top$Term,
+                                               levels = goterm_results_top$Term[
+                                                 order(goterm_results_top$padj, decreasing = TRUE)])
+
+  goterm_results_top %>% data.frame() %>% ggplot2::ggplot(aes(x = Term, y = -log10(padj))) +
+    geom_point() +
+    coord_flip() +
+    labs(title = plot_title,
+         x = "GO terms",
+         y = expression(-log[10](adjusted~italic(P)~value)))+
+    theme_minimal()
+}
+
+gotermAnalysis_results <- gotermAnalysis(resultsdge)
+gotermPlot_results <- gotermPlot(gotermAnalysis_results, plot_title = "Top 10 enriched GO-terms")
+
+
+gotermAnalysis_results %>% saveRDS(here("gotermAnalysis_results"))
+gotermPlot_results %>% saveRDS(here("gotermPlot_results"))
+
+a <- readRDS(here("gotermAnalysis_results"))
+class(a)
+?results
